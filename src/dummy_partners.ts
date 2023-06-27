fd64f8cb6e8001e8310b9cbe645494b092184186
Norbert B <dnorbertb@gmail.com>
2023-06-27 06:18:19 +0200

    Changed color scheme

--- src/dummy_partners.ts
+++ src/dummy_partners.ts
@@ -5,7 +5,11 @@ interface IPartner {
         url: string,
         alt: string
     },
-    accent_color: string,
+    accent_color: {
+        h: number
+        s: number
+        l: number
+    },
 }
 
 export const partners: IPartner[] = [
@@ -16,7 +20,11 @@ export const partners: IPartner[] = [
             url: 'https://www2.bac-assets.com/homepage/spa-assets/images/assets-images-global-logos-bac-logo-v2-CSX3648cbbb.svg',
             alt: 'Bank of America'
         },
-        accent_color: 'hsl(0, 100%, 50%)'
+        accent_color: {
+            h: 0,
+            s: 100,
+            l: 50
+        }
     },
     {
         id: 1,
@@ -25,7 +33,11 @@ export const partners: IPartner[] = [
             url: 'https://www.pngall.com/wp-content/uploads/2/B-Letter-Transparent.png',
             alt: 'Bizee'
         },
-        accent_color: 'hsl(24, 100%, 50%)'
+        accent_color: {
+            h: 24,
+            s: 100,
+            l: 50
+        }
     },
     {
         id: 2,
@@ -34,6 +46,10 @@ export const partners: IPartner[] = [
             url: 'https://upload.wikimedia.org/wikipedia/commons/d/de/HBO_logo.svg',
             alt: 'HBO',
         },
-        accent_color: 'hsl(300, 100%, 72%)'
+        accent_color: {
+            h: 300,
+            s: 100,
+            l: 72
+        }
     }
 ]
